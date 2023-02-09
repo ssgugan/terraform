e149d179b38239643b846421aab0bc8e78cbf18a
Gugan Thiruvengadam <gugan.thiruvengadam@htcindia.local>
2023-02-09 16:49:40 +0530

    test

--- variables.tf
+++ variables.tf
@@ -2,6 +2,14 @@ variable "region" {
   default = "us-east-2"
 }
 
+variable "access_key" {
+  default = "AKIA5S5YVN2L25NZEFXY"
+}
+
+variable "secret_key" {
+  default = "AVwOLZX+c07yUahI/sBscQTkoffiJGABuvC3GnUM"
+}
+
 variable "ami" {
   default = "ami-05bfbece1ed5beb54"
 }
