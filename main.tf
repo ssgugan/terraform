374ad6320795a6136ebe896cd02ff678adc84350
ssgugan <102643984+ssgugan@users.noreply.github.com>
2023-02-10 18:34:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,9 @@ provider "aws" {
 	secret_key = var.secret_key
 }
 
-data "aws_key_pair" "key" {
-    key_name = "ohio"
-}
-
 resource "aws_instance" "webserver" {
   ami = var.ami
   instance_type = var.instance_type
-  key_name = data.aws_key_pair.key.key_name
   tags = {
       Name = "test"
   } 
