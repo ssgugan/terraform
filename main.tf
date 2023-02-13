f63ffe0a7fee5fd74f3e6af848781b2057cb387e
ssgugan <102643984+ssgugan@users.noreply.github.com>
2023-02-13 12:25:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "webserver" {
   ami = var.ami
   instance_type = var.instance_type
+  subnet_id     = "subnet-07152dfe960fc56a1"
   tags = {
       Name = "test"
   } 
